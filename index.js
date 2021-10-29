174e647c9c4ed4a085d4b60b2c2c7252c27d2d77
Thomas Ismaïl Murzyn <thomasismailmurzyn@MacBook-Pro-de-Thomas.local>
2021-10-29 18:35:51 +0200

    +

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require("dotenv").config();
+
 const express = require("express");
 const formidable = require("express-formidable");
 const cors = require("cors");
-require("dotenv").config();
+
 const app = express();
 app.use(cors());
 app.use(formidable());
